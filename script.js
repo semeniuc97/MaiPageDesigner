731539190ebba1adbaac15245a9a9e9adecc5e2d
semeniuc97 <semeniuc97@gmail.com>
2019-01-17 18:55:05 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+    function AppViewModel() {
+        this.firstName = "Bert";
+        this.lastName = "Bertington";
+    }
+    
+    // Activates knockout.js
+    ko.applyBindings(new AppViewModel());
+    
